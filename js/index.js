32bf0a41794f58b25df79ba2d3ed7c74d2f72706
Drew McInnis <apmcinnis@gmail.com>
2016-03-13 15:54:03 -0500

    update

--- js/index.js
+++ js/index.js
@@ -1,18 +1,1 @@
-$(document).ready(function() {
-  $("h1").addClass("animated bounce");
-  $("#link1").click(function () {
-    $('html, body').animate({
-      scrollTop: $("#home").offset(200).top
-    }, 200);
-  });
-  $("#link2").click(function () {
-    $('html, body').animate({
-      scrollTop: $("#projects").offset().top
-    }, 200);
-  });
-  $("#link3").click(function () {
-    $('html, body').animate({
-      scrollTop: $("#about").offset().top
-    }, 200);
-  });
-});
+// Experiment with plain white BG & font-awesome icon color
